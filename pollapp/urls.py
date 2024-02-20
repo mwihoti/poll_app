25440563d3b4a37ae0b604bc2811076485399a4d
mwihoti <86232216+mwihoti@users.noreply.github.com>
2024-02-21 00:09:10 +0300

    vote views & detail template

--- pollapp/urls.py
+++ pollapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "pollapp"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("specifics/<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
     path("<int:question_id>/like/", views.like, name='like' )
